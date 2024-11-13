b10fbb769198db4b5d3a139100d268c1b8e8e32c
REN CHEN <cncojn.ren@gmail.com>
2024-11-14 06:21:48 +0800

    w/o specifying linux build

--- application.go
+++ application.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "test CI CD pipeline w/o bin",
+			"message": "test CI CD pipeline w/o bin w/o specifying linux build",
 			"deploy":  "success",
 		})
 	})
