4e256e8afb36e1599230f096407b1a28e9f5c5a7
REN CHEN <cncojn.ren@gmail.com>
2024-11-13 16:42:25 +0800

    replaced by aws default go application

--- application.go
+++ application.go
@@ -1,20 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// test code-pipeline cicd
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "Hello World! Append a name to the URL to say hello. For example, use %s/Mary to say hello to Mary.", r.Host)
-	} else {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":80", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	f, _ := os.Create("/var/log/golang/golang-server.log")
+	defer f.Close()
+	log.SetOutput(f)
+
+	const indexPage = "public/index.html"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			if _, err := ioutil.ReadAll(r.Body); err == nil {
+				log.Print("Received a message.\n")
+			}
+		} else {
+			log.Printf("Serving %s to %s...\n", indexPage, r.RemoteAddr)
+			http.ServeFile(w, r, indexPage)
+		}
+	})
+
+	http.HandleFunc("/scheduled", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			log.Printf("Received task %s scheduled at %s\n", r.Header.Get("X-Aws-Sqsd-Taskname"), r.Header.Get("X-Aws-Sqsd-Scheduled-At"))
+		}
+	})
+
+	log.Printf("Listening on port %s\n\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
